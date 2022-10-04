00bc1d889fdbb6194c788bf1b4857e9a047d046d
keeko <787018661@qq.com>
2022-10-04 09:06:43 +0800

    [feat] ts类库 编译产物引入(esm格式成功)

--- src/moduleTest/js-financial-tools.js
+++ src/moduleTest/js-financial-tools.js
@@ -1,124 +1,119 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.jsFinancialTools = factory());
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.jsFinancialTools = factory());
 })(this, (function () { 'use strict';
 
-  function isRealNumber(num) {
-      return typeof num === "number" && !isNaN(num);
-  }
-  function float(num, precision, placeholder = "--") {
-      const accuracy = isRealNumber(precision) ? precision : 2; // default digit
-      if (!isRealNumber(num)) {
-          return placeholder;
-      }
-      return num.toFixed(accuracy);
-  }
-  function percentage(num, precision, placeholder = "--") {
-      const accuracy = isRealNumber(precision) ? precision : 2;
-      if (!isRealNumber(num)) {
-          return placeholder;
-      }
-      return `${(num * 100).toFixed(accuracy)}%`;
-  }
+    function isRealNumber(num) {
+        return typeof num === "number" && !isNaN(num);
+    }
+    function float(num, precision, placeholder = "--") {
+        const accuracy = isRealNumber(precision) ? precision : 2; // default digit
+        if (!isRealNumber(num)) {
+            return placeholder;
+        }
+        return num.toFixed(accuracy);
+    }
+    function percentage(num, precision, placeholder = "--") {
+        const accuracy = isRealNumber(precision) ? precision : 2;
+        if (!isRealNumber(num)) {
+            return placeholder;
+        }
+        return `${(num * 100).toFixed(accuracy)}%`;
+    }
 
-  function getIntPartLength(num) {
-      return Math.floor(Math.log10(Math.abs(num))) + 1;
-  }
+    function getIntPartLength(num) {
+        return Math.floor(Math.log10(Math.abs(num))) + 1;
+    }
 
-  function formatRank(val) {
-      if (!isRealNumber(val)) {
-          return "--";
-      }
-      return `No.${val}`;
-  }
-  function formatLongText(val, limit) {
-      if (!val) {
-          return "--";
-      }
-      if (val.length > limit) {
-          return `${val.slice(0, limit)}...`;
-      }
-      return val;
-  }
-  function formatWithUnit(val, unitStr = "", precision = 2) {
-      if (!isRealNumber(val)) {
-          return "--";
-      }
-      let numStr;
-      switch (unitStr) {
-          case "万":
-              numStr = float(val / 10 ** 4, precision);
-              break;
-          case "亿":
-              numStr = float(val / 10 ** 8, precision);
-              break;
-          default:
-              numStr = String(val);
-              break;
-      }
-      return `${numStr}${unitStr}`;
-  }
-  function formatToMonetaryShape(val, precision = 2) {
-      if (!isRealNumber(val)) {
-          return "--";
-      }
-      const intPartLength = getIntPartLength(val);
-      if (intPartLength > 8) {
-          const num = val / 10 ** 8;
-          return `${float(num, precision)}亿`;
-      }
-      if (intPartLength > 4) {
-          const num = val / 10 ** 4;
-          return `${float(num, precision)}万`;
-      }
-      return `${float(val, precision)}`;
-  }
-  function formatToFloat(val, plusSign = "", precision = 2, scale = 1) {
-      if (!isRealNumber(val)) {
-          return "--";
-      }
-      const num = val / scale;
-      if (num > 0) {
-          return `${plusSign}${float(num, precision)}`;
-      }
-      return float(num, precision);
-  }
-  function formatToPercent(val, plusSign = "", precision = 2, scale = 1) {
-      if (!isRealNumber(val)) {
-          return "--";
-      }
-      const num = val / scale;
-      if (num > 0) {
-          return `${plusSign}${percentage(num, precision)}`;
-      }
-      return percentage(num, precision);
-  }
+    function formatRank(val) {
+        if (!isRealNumber(val)) {
+            return "--";
+        }
+        return `No.${val}`;
+    }
+    function formatLongText(val, limit) {
+        if (!val) {
+            return "--";
+        }
+        if (val.length > limit) {
+            return `${val.slice(0, limit)}...`;
+        }
+        return val;
+    }
+    function formatWithUnit(val, unitStr = "", precision = 2) {
+        if (!isRealNumber(val)) {
+            return "--";
+        }
+        let numStr;
+        switch (unitStr) {
+            case "万":
+                numStr = float(val / Math.pow(10, 4), precision);
+                break;
+            case "亿":
+                numStr = float(val / Math.pow(10, 8), precision);
+                break;
+            default:
+                numStr = String(val);
+                break;
+        }
+        return `${numStr}${unitStr}`;
+    }
+    function formatToMonetaryShape(val, precision = 2) {
+        if (!isRealNumber(val)) {
+            return "--";
+        }
+        const intPartLength = getIntPartLength(val);
+        if (intPartLength > 8) {
+            const num = val / Math.pow(10, 8);
+            return `${float(num, precision)}亿`;
+        }
+        if (intPartLength > 4) {
+            const num = val / Math.pow(10, 4);
+            return `${float(num, precision)}万`;
+        }
+        return `${float(val, precision)}`;
+    }
+    function formatToFloat(val, plusSign = "", precision = 2, scale = 1) {
+        if (!isRealNumber(val)) {
+            return "--";
+        }
+        const num = val / scale;
+        if (num > 0) {
+            return `${plusSign}${float(num, precision)}`;
+        }
+        return float(num, precision);
+    }
+    function formatToPercent(val, plusSign = "", precision = 2, scale = 1) {
+        if (!isRealNumber(val)) {
+            return "--";
+        }
+        const num = val / scale;
+        if (num > 0) {
+            return `${plusSign}${percentage(num, precision)}`;
+        }
+        return percentage(num, precision);
+    }
 
-  var formatter = /*#__PURE__*/Object.freeze({
-    __proto__: null,
-    formatRank: formatRank,
-    formatLongText: formatLongText,
-    formatWithUnit: formatWithUnit,
-    formatToMonetaryShape: formatToMonetaryShape,
-    formatToFloat: formatToFloat,
-    formatToPercent: formatToPercent
-  });
+    var formatter = /*#__PURE__*/Object.freeze({
+        __proto__: null,
+        formatRank: formatRank,
+        formatLongText: formatLongText,
+        formatWithUnit: formatWithUnit,
+        formatToMonetaryShape: formatToMonetaryShape,
+        formatToFloat: formatToFloat,
+        formatToPercent: formatToPercent
+    });
 
-  var display = /*#__PURE__*/Object.freeze({
-    __proto__: null,
-    formatter: formatter
-  });
+    var display = /*#__PURE__*/Object.freeze({
+        __proto__: null,
+        formatter: formatter
+    });
 
-  // import * as calc from "./calc";
-  // import * as util from "./util";
-  var index = {
-      // ...calc,
-      ...display,
-      // ...util
-  };
+    // import * as calc from "./calc";
+    // import * as util from "./util";
+    var index = Object.assign({}, display);
 
-  return index;
+    return index;
 
 }));
-//# sourceMappingURL=js-financial-tools.js.map
--- src/view/case/case2/Case2.tsx
+++ src/view/case/case2/Case2.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import jsFinancialTools from "@/moduleTest/jsFinancialTools";
+// import jsFinancialTools = require('@/moduleTest/js-financial-tools.js'); // vite 不支持 cjs以及umd格式 的编译产物
+import { formatToPercent } from '@/moduleTest/formatter'; // esm格式的编译产物没有问题
 
 type ContentInfo = {
   name: string;
   age: number;
   rank: number;
-}
+};
 
 const Case2 = () => {
   const [contentInfo, setContentInfo] = useState<ContentInfo>({} as ContentInfo);
@@ -17,11 +18,8 @@ const Case2 = () => {
   return (
     <>
       showCase2
-      <div>
-        {
-
-        }
-      </div>
+      {/* <div>{jsFinancialTools.formatter.formatToPercent(0.233578, '+', 3)}</div> */}
+      <div>{formatToPercent(0.233578, '+', 3)}</div>
     </>
   );
 };
