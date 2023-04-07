bf12d17098fec3be81f68913c63adcdd6f298039
keeko <787018661@qq.com>
2023-04-07 22:00:36 +0800

    [chore] 给 svg 设置 viewBox属性(这样尺寸改变才能生效)

--- src/view/case/case6/Case6.tsx
+++ src/view/case/case6/Case6.tsx
@@ -7,7 +7,11 @@ const Case6 = () => {
   return (
     <>
       hello, svg!
-      {/* 遇到一个问题 -> 使用vite-plugin-svgr 导入的svg标签 改变width和height对图标本身不生效 */}
+      {/* 
+        遇到一个问题 -> 使用vite-plugin-svgr 导入的svg标签 改变width和height对图标本身不生效 
+        
+        这不是vite-plugin-svgr的问题，是svg图标有问题，需要给 svg 设置 viewBox属性 才能设置尺寸
+      */}
       <SvgIcon
         iconName="icon_accNav"
         svgProp={{ width: 50, height: 50, fill: '#61dafb' }}
