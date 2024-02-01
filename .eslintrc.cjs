0f36f75bcbd37442a89a424e88abe70ab5da3afd
Keeko <787018661@qq.com>
2024-02-01 16:03:54 +0800

    [feat] react项目eslint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,49 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'eslint-config-prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2022,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'prettier'],
   rules: {
     'prettier/prettier': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^react', '^@?\\w'],
+          ['^(@|components)(/.*|$)'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ['^.+\\.?(css)$'],
+          ['^\\u0000']
+        ]
+      }
+    ],
+    'simple-import-sort/exports': 'error'
   },
   settings: {
     react: {
       createClass: 'createReactClass',
       pragma: 'React',
       fragment: 'Fragment',
-      version: 'detect',
-    },
-  },
+      version: 'detect'
+    }
+  }
 };
