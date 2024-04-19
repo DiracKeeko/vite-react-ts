a1f1ee47f98a04400239483776b837c5df2c79de
keeko <787018661@qq.com>
2024-04-19 09:26:50 +0800

    使用DynamicComp 传参

--- src/view/operation/debugModal/DebugModal.tsx
+++ src/view/operation/debugModal/DebugModal.tsx
@@ -8,8 +8,7 @@ import React, {
 import { Button, Modal as AntdModal, Spin, Steps } from 'antd';
 import { ModalProps as AntdModalProps } from 'antd';
 
-import Step1 from "./Step1";
-import Step2 from "./Step2";
+import DynamicComponent from './DynamicComponent';
 
 interface Options {
   afterShowModal?(): void | Promise<void>;
@@ -27,23 +26,23 @@ export interface ModalProps extends Omit<AntdModalProps, 'onOk' | 'onCancel'> {
 const stepArr = [
   {
     title: '1',
-    component: <Step1 />
+    component: 'Step1'
   },
   {
     title: '2',
-    component: <Step2 />
+    component: 'Step2'
   },
   {
     title: '3',
-    component: "hei Step3"
+    component: 'Step3'
   },
   {
     title: '4',
-    component: "hello Step4"
+    component: 'Step4'
   },
   {
     title: '5',
-    component: "yeah Step5"
+    component: 'Step5'
   }
 ];
 
@@ -60,6 +59,8 @@ const DebugModal: ForwardRefRenderFunction<ModalRef, ModalProps> = (props, ref) 
   const handlePrev = () => {
     setCurrentStep(currentStep - 1);
   };
+  const [prevDisabled, setPrevDisabled] = useState<boolean>(false);
+  const [nextDisabled, setNextDisabled] = useState<boolean>(false);
 
   const wrapOnOk = useCallback(
     async (event: React.MouseEvent<HTMLElement>) => {
@@ -98,14 +99,32 @@ const DebugModal: ForwardRefRenderFunction<ModalRef, ModalProps> = (props, ref) 
       onCancel={wrapOnCancel}
     >
       <Spin spinning={spinning}>
-        <Steps size="small" current={currentStep} items={stepArr.map((item) => ({ title: item.title }))} />
+        <Steps
+          size="small"
+          current={currentStep}
+          items={stepArr.map((item) => ({ title: item.title }))}
+        />
         <div>
-          {stepArr[currentStep].component}
+          <DynamicComponent
+            component={stepArr[currentStep].component}
+            setPrevDisabled={setPrevDisabled}
+            setNextDisabled={setNextDisabled}
+          ></DynamicComponent>
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div>{currentStep !== 0 && <Button onClick={handlePrev}>上一步</Button>}</div>
           <div>
-            {currentStep !== stepArr.length - 1 && <Button onClick={handleNext}>下一步</Button>}
+            {currentStep !== 0 && (
+              <Button disabled={prevDisabled} onClick={handlePrev}>
+                上一步
+              </Button>
+            )}
+          </div>
+          <div>
+            {currentStep !== stepArr.length - 1 && (
+              <Button disabled={nextDisabled} onClick={handleNext}>
+                下一步
+              </Button>
+            )}
           </div>
         </div>
       </Spin>
