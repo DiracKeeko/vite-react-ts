f093c1eead989184f915f52d31a30571ed743b16
Keeko <787018661@qq.com>
2024-12-27 15:25:53 +0800

    [feat] 更新 vite-plugin-svgr@4.3.0 语法

--- src/view/case/case6/Case6.tsx
+++ src/view/case/case6/Case6.tsx
@@ -1,6 +1,9 @@
-import SvgIcon from '@/component/SvgIcon';
+import React from 'react';
 
-import { ReactComponent as IconAccNav } from '@/icon/icon_accNav.svg';
+import SvgIcon from '@/component/SvgIcon';
+// import { ReactComponent as IconAccNav } from '@/icon/icon_accNav.svg'; // vite-plugin-svgr@2.4.0 语法
+// import iconAcc from '@/icon/icon_accNav.svg'; // 静态资源引入 + 放入<img />使用
+import IconAccNav from '@/icon/icon_accNav.svg?react'; // vite-plugin-svgr@4.3.0 语法
 
 const Case6 = () => {
   return (
@@ -11,12 +14,12 @@ const Case6 = () => {
         
         这不是vite-plugin-svgr的问题，是svg图标有问题，需要给 svg 设置 viewBox属性 才能设置尺寸
       */}
-      <SvgIcon
-        iconName="icon_accNav"
-        svgProp={{ width: 50, height: 50, fill: '#61dafb' }}
-      />
+      <SvgIcon iconName="icon_accNav" svgProp={{ width: 50, height: 50, fill: '#61dafb' }} />
       {/* ↓ 这是原始的 vite-plugin-svgr 的使用方式 */}
-      <IconAccNav/>
+      <IconAccNav />
+
+      {/* ↓ 这是最原始的 svg资源 的使用方式 */}
+      {/* <img src={iconAcc} alt="Icon" /> */}
     </>
   );
 };
