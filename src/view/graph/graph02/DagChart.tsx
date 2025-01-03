38111ec07154098b6e414342978daa6d66077b8d
Keeko <787018661@qq.com>
2025-01-03 10:47:30 +0800

    [feat] 定义行为, 注册行为, 注册节点

--- src/view/graph/graph02/DagChart.tsx
+++ src/view/graph/graph02/DagChart.tsx
@@ -1,39 +1,79 @@
-import React, { useEffect, useRef } from 'react';
-import { Graph } from '@antv/g6';
+import React, { useEffect } from 'react';
+import { ExtensionCategory, Graph, HoverActivate, idOf, register } from '@antv/g6';
+import { NodeData } from '@antv/g6/lib/spec';
+import { ReactNode } from '@antv/g6-extension-react';
 
 import Node from './Node';
 
 const ACTIVE_COLOR = '#f6c523';
 
-export const PerformanceDiagnosisFlowchart: React.FC = () => {
-  const containerRef = useRef<HTMLDivElement | null>(null);
+class HoverElement extends HoverActivate {
+  getActiveIds(event: any) {
+    const { model, graph } = this.context;
+    const { targetType, target } = event;
+    const targetId = target.id;
 
+    const ids = [targetId];
+    if (targetType === 'edge') {
+      const edge = model.getEdgeDatum(targetId);
+      ids.push(edge.source, edge.target);
+    } else if (targetType === 'node') {
+      ids.push(...model.getRelatedEdgesData(targetId).map(idOf));
+    }
 
+    graph.frontElement(ids);
+
+    return ids;
+  }
+}
+
+register(ExtensionCategory.NODE, 'react', ReactNode);
+register(ExtensionCategory.BEHAVIOR, 'hover-element', HoverElement);
+
+type SizeMap = {
+  [key: string]: [number, number];
+  preInspection: [number, number];
+  problem: [number, number];
+  inspection: [number, number];
+  solution: [number, number];
+  default: [number, number];
+};
+
+interface DagChartProps {
+  containerId: string; // G6 图表容器 ID
+  nodesData: NodeData[]; // 节点数据
+  edgesData: { source: string; target: string }[]; // 边数据
+}
+
+const DagChart = ({ containerId, nodesData, edgesData }: DagChartProps) => {
   useEffect(() => {
     const graphInstance: Graph | null = new Graph({
-      container: containerRef.current!,
-      data,
+      container: containerId,
+      data: {
+        nodes: nodesData,
+        edges: edgesData
+      },
       autoFit: 'view',
       node: {
         type: 'react',
-        style: (d) => {
-          const style = {
-            component: <Node data={d} />,
-            ports: [{ placement: 'top' }, { placement: 'bottom' }]
-          };
-
-          const size = {
-            'pre-inspection': [240, 120],
+        style: (d: NodeData) => {
+          const curType: string = (d.data?.type as string) || 'default';
+          const sizeMap: SizeMap = {
+            preInspection: [240, 120],
             problem: [200, 120],
             inspection: [330, 100],
-            solution: [200, 120]
-          }[d.data.type] || [200, 80];
+            solution: [200, 120],
+            default: [200, 80]
+          };
+          const size = sizeMap[curType];
 
-          Object.assign(style, {
+          const style = {
+            component: <Node data={d} />,
+            ports: [{ placement: 'top' }, { placement: 'bottom' }],
             size,
             dx: -size[0] / 2,
             dy: -size[1] / 2
-          });
+          };
           return style;
         },
         state: {
@@ -88,7 +128,7 @@ export const PerformanceDiagnosisFlowchart: React.FC = () => {
     };
   }, []);
 
-  return <div ref={containerRef} style={{ width: '100%', height: '100%' }}></div>;
+  return <div id={containerId} style={{ width: '100%', height: '100%' }}></div>;
 };
 
-export default PerformanceDiagnosisFlowchart;
+export default DagChart;
